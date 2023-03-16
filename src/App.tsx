2983d3e9016da88c9f802e832871ad52da04433d
handwoong <handwoong@gmail.com>
2023-03-17 02:02:00 +0900

    :wrench: chore: Set eslint, prettier, alias

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React from 'react';
+import { SWRConfig } from 'swr';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <SWRConfig
+      value={{
+        onError: (error) => {
+          toast.error(error.message);
+        },
+      }}
+    >
+      <div className="App"></div>;
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+    </SWRConfig>
+  );
 }
 
 export default App;
