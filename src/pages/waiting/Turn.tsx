1faddbefc21d98c01b44bacc6dd3c3532cb563fb
handwoong <handwoong@gmail.com>
2023-04-11 22:34:57 +0900

    :sparkles: feat: Add waiting turn description

--- src/pages/waiting/Turn.tsx
+++ src/pages/waiting/Turn.tsx
@@ -10,7 +10,10 @@ export function Turn() {
   const { waitingId } = useParams();
   const navigate = useNavigate();
   const [isMounted, setMounted] = useState(false);
-  const { data, isLoading } = useSWR<WaitingResponse>(isMounted ? `/api/waiting/turn/${waitingId}` : null, getFetcher);
+  const { data, isLoading } = useSWR<WaitingResponse>(
+    isMounted ? `/api/waiting/turn/${waitingId}` : null,
+    getFetcher,
+  );
 
   useEffect(() => {
     if (!waitingId || waitingId?.length !== 36) {
@@ -20,7 +23,7 @@ export function Turn() {
     setMounted(true);
   }, [waitingId, navigate]);
 
-  if (isLoading) return <AppSpinner />
+  if (isLoading) return <AppSpinner />;
 
   return (
     <div className="container col-xl-10 col-xxl-8 px-4 py-5">
@@ -30,10 +33,19 @@ export function Turn() {
             <h1 className="display-4 fw-bold">대기번호 : {data?.waitingNumber}번</h1>
             <div className="col-lg-6 mx-auto">
               <h2 className="lead mb-4">내 앞 대기팀 : {data?.waitingTurn}팀</h2>
+              {data?.waitingTurn === 0 ? (
+                <p>
+                  빈 테이블이 없거나 테이블을 정리중입니다.
+                  <br />
+                  준비가 완료되면 입장 메시지를 전송해드릴게요.
+                </p>
+              ) : null}
             </div>
           </>
         ) : (
-          <h1 className="display-4 fw-bold">입장이 완료되었거나 취소된 대기 번호 입니다.</h1>
+          <h1 className="display-4 fw-bold">
+            입장이 완료되었거나 취소된 대기 번호 입니다.
+          </h1>
         )}
       </div>
     </div>
